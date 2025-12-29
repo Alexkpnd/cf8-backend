d24e3503d62dca6d43946c6c6be3537a9f036aa8
Alkapandelis <alexkpnd@gmail.com>
2025-12-29 15:57:52 +0200

    Feat: add Role List endpoint

---
+++ src/controllers/role.controller.ts
@@ -0,0 +1,11 @@
+import {Request, Response, NextFunction} from 'express';
+import * as rolesService from '../services/role.service';
+
+export const list = async(req:Request, res:Response, next:NextFunction) => {
+    try{
+        const result = await rolesService.findAllRoles();
+        res.status(200).json(result);
+    } catch (err) {
+        next(err);
+    }
+}
