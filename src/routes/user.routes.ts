0cb8fafb68d467d825b2f6b4309397fe481b8c56
Alkapandelis <alexkpnd@gmail.com>
2026-01-04 17:25:54 +0200

    Feat: Swagger for  documentation

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -8,8 +8,50 @@ import { updateUserSchema } from "../validators/user.validator";
 import { authenticate } from "../middlewares/auth.middleware";
 import { hasAdminRole } from "../middlewares/user.middleware";
 
+/**
+ * @openapi
+ * /users:
+ *  get:
+ *      summary: List of all users
+ *      tags: 
+ *          [Users]
+ *      security: 
+ *          - bearerAuth: []
+ *      responses:
+ *          200:
+ *              description: Response list of users
+ */
 router.get("/", authenticate, hasAdminRole, userCtrl.list);
 router.get('/:id', validateObjectId(), userCtrl.getOne);
+/**
+ * @openapi
+ * /users:
+ *  post:
+ *      summary: Creates a user
+ *      tags: [Users]
+ *      security:
+ *          - bearerAuth: []
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *              schema:
+ *                  type: object
+ *                  properties:
+ *                      username:
+ *                          type: string
+ *                      password:
+ *                          type: string
+ *                      firstname:
+ *                          type: string
+ *                      lastname:
+ *                          type: string
+ *                      email:
+ *                          type: string
+ *      responses:
+ *          201: 
+ *              description: User Created
+ */
 router.post('/', authenticate, validate(createUserSchema), userCtrl.create);
 router.put('/:id', authenticate, validate(updateUserSchema), validateObjectId(), userCtrl.update);
 router.delete('/:id', authenticate, validateObjectId('id'), userCtrl.remove);
