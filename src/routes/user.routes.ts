d778a841bc9100190965514bfda6d0e8190ab973
Alkapandelis <alexkpnd@gmail.com>
2026-01-04 02:32:19 +0200

    authenticate middleware

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -5,12 +5,13 @@ import {validate} from '../middlewares/validate.middleware';
 import { createUserSchema } from "../validators/user.validator";
 import { validateObjectId } from "../middlewares/validateObjectId.middleware";
 import { updateUserSchema } from "../validators/user.validator";
+import { authenticate } from "../middlewares/auth.middleware";
 
-router.get("/",userCtrl.list);
+router.get("/", authenticate, userCtrl.list);
 router.get('/:id', validateObjectId(), userCtrl.getOne);
-router.post('/', validate(createUserSchema), userCtrl.create);
-router.put('/:id', validate(updateUserSchema), validateObjectId(), userCtrl.update);
-router.delete('/:id', validateObjectId('id'), userCtrl.remove);
+router.post('/', authenticate, validate(createUserSchema), userCtrl.create);
+router.put('/:id', authenticate, validate(updateUserSchema), validateObjectId(), userCtrl.update);
+router.delete('/:id', authenticate, validateObjectId('id'), userCtrl.remove);
 
 
 export default router;
